4bbc2d5aefa56624bd29ed6d1432e10f70885e3c
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-03-21 22:32:37 +0900

    不要な部分をコメントアウト

--- python/waveform.py
+++ python/waveform.py
@@ -43,45 +43,29 @@ if __name__ == "__main__":
     print("Damping Coefficient: {}".format(damping_coeff))
     """
 
-    m = 0.5
-    k = 1000.
-    c = 5.
-    cc = 2. * np.sqrt(m * k)    # 臨界減衰比
+    m = 0.5     # 質量 [kg]
+    k = 1000.   # 剛性 [N/m]
+    c = 5.      # 減衰係数 [N･s/m]
+    #cc = 2. * np.sqrt(m * k)    # 臨界減衰比
+    
+    """
     zeta = c / cc
     omega = np.sqrt(k/m)
-    omeda_d = omega * np.sqrt(1 - np.power(zeta, 2.))
+    omega_d = omega * np.sqrt(1 - np.power(zeta, 2.))
     sigma = omega_d * zeta
+    """
 
-    f = 7. 
+    f = 7.               # 周波数 [Hz]
     state0 = [0.1, 0.]   # 初期値 [初期変位, 初速]
 
-    n_order_v = state0[1] / (state0[0] * omega)
+    # n_order_v = state0[1] / (state0[0] * omega)
  
-    # 減衰振動の場合
-    if zeta < 1:
-        """減衰振動
-        """
-        X = np.sqrt(np.power(state0[0], 2) + np.power((state0[1] + sigma * state0[0])/omega_d, 2))
-        phi = np.arctan((state0[1] + (sigma * state0[0]))/(state0[0] * omega_d))
-        theory = np.exp(- sigma * t) * X * np.cos(omega_d * t - phi)
-    elif zeta == 1:
-        """臨界減衰
-        """
-        theory = state0[0] * np.exp(- omega * t) * ((n_order_v + 1) * omega * t + 1)
-    else:
-        """過減衰
-        """
-        theory = state0[0] * np.exp(- zeta * omega * t) * (\
-            np.cosh(omega * t * np.sqrt(zeta ** 2 - 1))\
-            + (n_order_v + zeta) / (np.sqrt(zeta ** 2 + 1))\
-            * np.sinh(omega * t * np.sqrt(zeta ** 2 - 1)))
-
     t0 = 0.         # 初期時刻 [s]
     tf = 10.        # 終了時刻 [s]
     dt = 1./48000.  # 時刻の刻み幅 [s]
     t = np.arange(0, tf, dt)
 
-    sol = odeint(func, state0, t, args(m, k, c))
+    sol = odeint(func, state0, t, args=(m, k, c))
     plot.plot(t, sol[:,0])
     plot.tight_layout()
     plot.show()
