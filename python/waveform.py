b11e05f438ac38a7eb25b76d8a6089e669b50287
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-03-21 23:34:30 +0900

    入出力を書き分ける
    Q値に名称を変更

--- python/waveform.py
+++ python/waveform.py
@@ -18,48 +18,27 @@ def func(state, t, m, k, c):
 
 if __name__ == "__main__":
     """周波数、重量、減衰比を与えれば機械の周波数応答を自動計算する
-    print("Normal mode(f0) frequency (Hz): ", end="")
-    f0 = float(input())
-    print("Mass (kg): ", end="")
-    mass = float(input())
-    print("Damping ratio [0,1]: ", end="")
-    damping_ratio = float(input())
-
-    # 臨界減衰比は下式で求められる
-    # f0 = sqrt(k/m)
-    # f0^2 = k/m
-    # f0^2 * m = k
-
-    # 減衰比は下式で求められる
-    # z = c / cc
-
-    k = f0**2. * mass
-    critical_damping = 2. * np.sqrt(mass * k)
-    damping_coeff = critical_damping / damping_ratio
-
-    print("--------------------------------------------------")
-    print("Spring constant (N): {}".format(k))
-    print("Critical damping ratio: {}".format(critical_damping))
-    print("Damping Coefficient: {}".format(damping_coeff))
-    """
-
-    m = 0.5     # 質量 [kg]
-    k = 1000.   # 剛性 [N/m]
-    c = 5.      # 減衰係数 [N･s/m]
-    #cc = 2. * np.sqrt(m * k)    # 臨界減衰比
+    Args以下のコメントはプログラムの入力に使われる変数
     
-    """
-    zeta = c / cc
-    omega = np.sqrt(k/m)
-    omega_d = omega * np.sqrt(1 - np.power(zeta, 2.))
-    sigma = omega_d * zeta
+    Args:
+        f0 (float): 固有振動数 [Hz]
+        m (float): 質量 [kg]
+        Q (float): Quarity Factor, Q値
+        tf (float): 終了時刻 [s]
     """
 
-    f = 7.               # 周波数 [Hz]
+    f0 = 7.            # 固有振動数 [Hz]
+    m = 0.5             # 質量 [kg]
+    Q = 20.             # Q値
+
+    k = f0 ** 2 * m             # 剛性 [N/m]
+    cc = 2. * np.sqrt(m * k)    # 臨界減衰係数
+    c = cc / Q                  # 減衰係数 [N･s/m]
+    
+    
+    
     state0 = [0.1, 0.]   # 初期値 [初期変位, 初速]
 
-    # n_order_v = state0[1] / (state0[0] * omega)
- 
     t0 = 0.         # 初期時刻 [s]
     tf = 10.        # 終了時刻 [s]
     dt = 1./48000.  # 時刻の刻み幅 [s]
