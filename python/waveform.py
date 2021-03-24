2d740a5ef5586dfd8ef05bcf48c4e87344a9ab38
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-03-24 21:07:41 +0900

    波形のジェネレータクラスを作る

--- python/waveform.py
+++ python/waveform.py
@@ -16,8 +16,71 @@ def func(state, t, m, k, c):
     dxdt = -(c/m) * x2 - (k/m) * x1
     return [x2, dxdt]
 
-def compute_waveform():
-    pass
+
+class Waveform:
+    dens = 1145.         # 弦の密度 [kg/m^3], 1145 kg/m^3 はナイロン弦
+    E = 5.4e9            # ヤング率 [GPa]
+
+    def __init__(self, spfq: int, f0: float, L0: float, Q: float, radius: float, ratio: float, tf: float):
+        """波形クラス
+
+        Args:
+            spfq (int): サンプリング周波数
+            f0 (float): 基本周波数 [Hz]
+            L0 (float): 弦の長さ [m]
+            Q (float): Q値
+            radius (float): 弦の半径 [m]
+            ratio (float): 弦を押さえた位置 [0,1]
+            tf (float): 終了時刻 [s]
+        """
+        dt = 1. / float(spfq)
+        t = np.arange(0, tf, dt)            # 時間の配列 [s]
+        A = radius * radius * np.pi         # 断面 [m^2]
+        m = A * L0 * Waveform.dens          # 質量 [kg]
+        rho = A * Waveform.dens             # 線密度 [kg/m]
+        sigma = (2. * L0 * f0)**2. * rho    # 応力 [Pa]
+        delta = (L0 * sigma) / E            # 引張後のひずみ
+        I = (radius * 2.)**4. * np.pi / 4   # 断面二次モーメント [m^4]
+        T = (2. * L0 * f0)**2. * rho        # 張力 [N]
+        B = (np.pi / (L0 + delta))**2. * ((E * I) / T)
+
+        self.__numof_harmonic = int((spfq / 2.) / f0) # 倍音の数
+        self.__ns = [n + 2 for n in range(numof_harmonic - 2)]
+
+        self.__spfq = spfq
+        self.__f0 = f0
+        self.__L0 = L0
+        self.__Q = Q
+        self.__dt = dt
+        self.__t = T
+        self.__A = A
+        self.__m = m
+        self.__rho = rho
+        self.__sigma = sigma
+        self.__delta = delta
+        self.__I = I
+        self.__T = T
+        self.__B = B
+
+    
+    def generate(self, v0: float):
+        state0 = [0., v0]
+
+        k0 = self.__f0**2. * self.__m       # 剛性 [N/m]
+        cc = 2. * np.sqrt(self.__m * k0)    # 臨界減衰係数
+        c0 = cc / self.__Q                  # 減衰係数 [N･s/m]
+        wave = np.array(odeint(func, state0, self.__t, args=(self.__m, k0, c0))[:,0])
+        harmonics = []
+
+        for n in ns:
+            fn = float(n) * f0 * np.sqrt(1. + self.__B * float(n)**2.) # 部分音周波数
+            kn = fn**2. * self.__m      # 剛性 [N/m]
+            cc = 2. * np.sqrt(m * kn)
+            cn = cc / self.__Q
+            hw = odeint(func, state0, self.__t, args(self.__m, kn, cn))[:,0]
+            wave += hw
+
+        return wave
 
 if __name__ == "__main__":
     """周波数、重量、減衰比を与えれば機械の周波数応答を自動計算する
@@ -36,13 +99,13 @@ if __name__ == "__main__":
     midi_freqs = [440. * 2.**((float(n)-69.)/12.) for n in range(128)]
 
     L0 = 0.5            # 弦の長さ [m]
-    Q = 2000.             # Q値
+    Q = 2000.           # Q値
     radius = 0.001      # 弦の半径 [m]
     ratio = 0.3         # 全体の長さから見た押さえた位置 [0,1]
 
     # 以下は定数とする
     dens = 1145.         # 弦の密度 [kg/m^3], 1145 kg/m^3 はナイロン弦
-    E = 5.4e9           # ヤング率 [GPa]
+    E = 5.4e9            # ヤング率 [GPa]
 
     # 固有振動数 [Hz]
     f0 = midi_freqs[selected_keynum]
@@ -87,6 +150,8 @@ if __name__ == "__main__":
     plot.plot(t, sol[:,0])
     plot.tight_layout()
     plot.show()
+
+    print(state0)
 
     """参考文献
     https://watlab-blog.com/2019/06/10/python-1dof-mck/
