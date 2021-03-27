02f14837e47565af8fded6c45a255e453e6d807c
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-03-27 20:08:01 +0900

    使わないパラメータを消してB値依存に変更

--- python/waveform.py
+++ python/waveform.py
@@ -23,80 +23,53 @@ class Waveform:
     dens = 1145.         # 弦の密度 [kg/m^3], 1145 kg/m^3 はナイロン弦
     E = 5.4e9            # ヤング率 [GPa]
 
-    def __init__(self, spfq: int, notenum: int, L0: float, Q: float, tf: float):
+    def __init__(self, spfq: int, notenum: int, B: float):
         """波形クラス
 
         Args:
             spfq (int): サンプリング周波数
             notenum (int): ノート番号
-            L0 (float): 弦の長さ [m]
-            Q (float): Q値
-            
-            tf (float): 終了時刻 [s]
+            B (float): B値
         """
         f0 = 440. * 2.**(float(notenum-69)/12.)
         radius = (-0.0019 * notenum + 0.1234) * 25.4 / 2. / 1000.
-
-        A = radius * radius * np.pi         # 断面 [m^2]
-        m = A * L0 * Waveform.dens          # 質量 [kg]
-        rho = A * Waveform.dens             # 線密度 [kg/m]
-        sigma = (2. * L0 * f0)**2. * rho    # 応力 [Pa]
-        delta = (L0 * sigma) / Waveform.E   # 引張後のひずみ
-        I = (radius * 2.)**4. * np.pi / 4   # 断面二次モーメント [m^4]
-        T = (2. * L0 * f0)**2. * rho        # 張力 [N]
-        B = (np.pi / (L0 + delta))**2. * ((Waveform.E * I) / T)
 
         self.__numof_harmonic = int((spfq / 2.) / f0) # 倍音の数
         self.__ns = [n + 2 for n in range(self.__numof_harmonic - 2)]
 
         self.__spfq = spfq
         self.__f0 = f0
-        self.__L0 = L0
-        self.__Q = Q
-        self.__tf = tf
         self.__dt = 1. / float(self.__spfq)
-        self.__t = np.arange(0, self.__tf, self.__dt)
-        self.__A = A
-        self.__m = m
-        self.__rho = rho
-        self.__sigma = sigma
-        self.__delta = delta
-        self.__I = I
-        self.__T = T
         self.__B = B
-        self.__radius = radius
-        print(self.__radius)
-        print(self.__f0)
-    
-    def get_t(self):
-        """時刻の配列を取得する
 
-        Returns:
-            np.ndarray: 時刻の配列
-        """
-        return self.__t
-
-    def __sinwave(self, freq: float, amp: float) -> np.ndarray:
-        return np.sin(2. * np.pi * freq * self.__t) * (1. / np.exp(self.__t * amp))
+    def __sinwave(self, t: np.ndarray, freq: float, amp: float) -> np.ndarray:
+        return np.sin(2. * np.pi * freq * t) * (1. / np.exp(t * amp))
     
-    def generate(self, v0: float, ratio: float) -> np.ndarray:
+    def generate(self, v0: float, ratio: float, start: float, end: float) -> np.ndarray:
         """波形を清々する
 
         Args:
-            v0 (float): 初期速度
+            v0 (float): 初期速度 [m/s]
+            start (float): 開始時間 [s]
+            end (float): 終了時間 [s]
             ratio (float): 弦を弾いた位置 [0,1]
 
         Returns:
             np.ndarray: 波形のnumpy配列
         """
+        t = np.arange(start, end, self.__dt)
         amp = 1.
-        wave = self.__sinwave(self.__f0, amp)
+        wave = self.__sinwave(t, self.__f0, amp)
 
         for ns in self.__ns:
             amp += 1.
-            wave += self.__sinwave(self.__f0 * ns, amp)
+            f_elastic = self.__f0 * ns * np.sqrt(1 + self.__B * ns ** 2.)
+            wave += self.__sinwave(t, f_elastic, amp)
 
         return wave
+    
+    def get_t(self, start: float, end: float) -> np.ndarray:
+        return np.arange(start, end, self.__dt)
 
 if __name__ == "__main__":
     """周波数、重量、減衰比を与えれば機械の周波数応答を自動計算する
@@ -112,18 +85,19 @@ if __name__ == "__main__":
     """
 
     notenum = 50
-    L0 = 0.5            # 弦の長さ [m]
-    Q = 2000.           # Q値
 
     ratio = 0.3         # 全体の長さから見た押さえた位置 [0,1]
     tf = 10.            # 終了時刻 [s]
     spfq = 8000.        # サンプリング周波数 [Hz]
     v0 = 0.05             # 初期速度
+    start = 0.
+    end = 10. 
+    B = 0.0001
 
-    sound = Waveform(spfq, notenum, L0, Q, tf)
-    waveform = sound.generate(v0, ratio)
+    sound = Waveform(spfq, notenum, B)
+    waveform = sound.generate(v0, ratio, start, end)
 
-    plot.plot(sound.get_t(), waveform)
+    plot.plot(sound.get_t(start, end), waveform)
 
     plot.tight_layout()
     plot.show()
