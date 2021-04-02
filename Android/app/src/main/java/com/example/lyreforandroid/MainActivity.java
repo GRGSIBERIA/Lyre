208879ff98823e580a3a1beded4ea2906a17515e
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-04-02 22:11:10 +0900

    音が出るところまで実装する

--- Android/app/src/main/java/com/example/lyreforandroid/MainActivity.java
+++ Android/app/src/main/java/com/example/lyreforandroid/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.lyreforandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.widget.Button;
 
 import android.media.AudioAttributes;
@@ -17,8 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
     private Button[] toneButtons;
     private Button[] octaveButtons;
+    private Button[] keyButtons;
     private AudioTrack[] tracks;
     private int selectedOctave;
+    private int selectedKey;
+    private int[] scale;
+    private boolean isMajor;
 
     final int SamplingRate = 48000;
 
@@ -26,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isMajor = true;
+        selectedOctave = 0;
+        selectedKey = 0;
+
+        scale = new int[] {
+            0, 2, 4, 5, 7, 9, 11
+        };
 
         toneButtons = new Button[] {
                 findViewById(R.id.button1),
@@ -43,12 +53,33 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.buttonOctave3)
         };
 
+        keyButtons = new Button[] {
+                findViewById(R.id.buttonC),
+                findViewById(R.id.buttonCs),
+                findViewById(R.id.buttonD),
+                findViewById(R.id.buttonDs),
+                findViewById(R.id.buttonE),
+                findViewById(R.id.buttonF),
+                findViewById(R.id.buttonFs),
+                findViewById(R.id.buttonG),
+                findViewById(R.id.buttonGs),
+                findViewById(R.id.buttonA),
+                findViewById(R.id.buttonAs),
+        };
+
+        Button isMajorBtn = findViewById(R.id.isMinor);
+        isMajorBtn.setOnClickListener(v -> {
+            isMajor = !isMajor;
+        });
+        isMajorBtn.setSoundEffectsEnabled(false);
+
         for (int i = 0; i < toneButtons.length; ++i)
         {
             int finalI = i;
             toneButtons[i].setOnClickListener(v -> {
                 wavPlay(finalI);
             });
+            toneButtons[i].setSoundEffectsEnabled(false);
         }
 
         for (int i = 0; i < octaveButtons.length; ++i)
@@ -57,7 +88,19 @@ public class MainActivity extends AppCompatActivity {
             octaveButtons[i].setOnClickListener(v -> {
                 selectedOctave = finalI;
             });
+            octaveButtons[i].setSoundEffectsEnabled(false);
         }
+
+        for (int i = 0; i < keyButtons.length; ++i)
+        {
+            int finalI = i;
+            keyButtons[i].setOnClickListener(v -> {
+                selectedKey = finalI;
+            });
+            keyButtons[i].setSoundEffectsEnabled(false);
+        }
+
+
 
         tracks = new AudioTrack[] {
                 BuildTrack(R.raw.a21),  // A0
@@ -134,9 +177,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // バッファサイズを取得
+        /* MODE_STATICの場合はバッファをストリーミングせずにリソースを使い回す
         int bufSize = android.media.AudioTrack.getMinBufferSize(
                 SamplingRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT
         );
+        */
 
         AudioTrack track = new AudioTrack.Builder()
                 .setAudioAttributes(new AudioAttributes.Builder()
@@ -148,16 +193,24 @@ public class MainActivity extends AppCompatActivity {
                         .setSampleRate(48000)
                         .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
                         .build())
-                .setBufferSizeInBytes(bufSize)
+                .setBufferSizeInBytes(wavData.length - 44)
+                .setTransferMode(AudioTrack.MODE_STATIC)
                 .build();
 
-        track.write(wavData, 44, wavData.length - 44);
+        // data+4バイト目以降の数百バイトが0xFFFFになって不安定
+        wavData[802] = 0; wavData[803] = 0;
+        track.write(wavData, 802, wavData.length - 802);
 
         return track;
     }
 
     private void wavPlay(int num)
     {
+        int majorUp = isMajor ? 3 : 0;
 
+        int toneId = selectedKey + majorUp + scale[num] + selectedOctave * 12;
+
+        tracks[toneId].stop();
+        tracks[toneId].play();
     }
 }
